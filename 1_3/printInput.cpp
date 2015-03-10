fe726ef8f29d953637982c9cb3918e77604e733a
Joe Blair <joemichaelblair@gmail.com>
2015-03-10 20:13:47 +0000

    Added program printInput which prints what the user inputs into the console

---
+++ 1_3/printInput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Enter a number: "; 
+	int x = 0;
+	//read number from console and stores it in x
+	std::cin >> x; 
+	std::cout << "You entered " << x << std::endl;
+	return 0;	
+}
